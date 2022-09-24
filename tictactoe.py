5c111896d18dbf061eac79b37b10ef023fa49da3
victornmeka <nme22001@byui.edu>
2022-09-24 18:40:56 +0100

    tictactoe

---
+++ tictactoe.py
@@ -0,0 +1,70 @@
+'''
+Tic-Tac-Toe
+"Love is a game of tic-tac-toe, constantly waiting for the next x or o." -Lang Leav-
+'''
+# Tic-Tac-Toe
+# coder: Victor Nmeka
+
+
+def main():
+    player = next_player("")
+    board = create_board()
+    print(f"\x1B[1m Welcome to Tic-Tac-Toe \x1B[0m \n\x1B[3m please follow the instructions.\x1B[0m\
+        \nHave A Swell Time!!!")
+    while not (there_is_a_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. !!")
+
+
+def create_board():
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
+
+
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+
+
+def is_a_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
+
+
+def there_is_a_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
+
+
+def make_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+
+if __name__ == "__main__":
+    main()
